d1d6452a908a8d132cccace34df9da83750823f5
Braily Guzman <braily@outlook.de>
2024-03-15 09:28:25 -0400

    Responsiveness and Styling Update
    ~ Fixed the responsiveness on smaller screens.
    ~ Removed unused styling.
    ~ Fixed styling issues.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import '../styles/Footer.css';
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; 2023 Syntax Car Wash. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()} Syntax Car Wash. All rights reserved.</p>
         </footer>
     );
 };
